0a97f7319e45384c9e8feeae85ed3657f1041987
James Pike <jpike@chilon.net>
2015-08-08 13:57:24 +0100

    Event is now a member of sigh-core rather than sigh

--- src/log.js
+++ src/log.js
@@ -1,18 +1,17 @@
 import util from 'util'
 
-var color = '\x1b[35m'
 var clear = '\x1b[0m'
 
 var log = (format, ...args) => {
-  console.log(` ${color}*${clear} ${format}`, ...args)
+  console.log(` ${log.color}*${clear} ${format}`, ...args)
 }
 
 log.nested = (format, ...args) => {
-  console.log(`   ${color}-${clear} ${format}`, ...args)
+  console.log(`   ${log.color}-${clear} ${format}`, ...args)
 }
 
 log.warn = (format, ...args) => {
-  console.warn(` ${color}!${clear} ${format}`, ...args)
+  console.warn(` ${log.color}!${clear} ${format}`, ...args)
 }
 
 log.important = (format, ...args) => {
@@ -21,9 +20,10 @@ log.important = (format, ...args) => {
   for (var i = 0; i < text.length + 7; ++i)
     lines += '*'
 
-  console.log(` ${color}*${lines}`)
-  console.log(` ${color}***${clear} ${text} ${color}***`)
-  console.log(` ${color}*${lines}`)
+  console.log(` ${log.color}*${lines}`)
+  console.log(` ${log.color}***${clear} ${text} ${log.color}***`)
+  console.log(` ${log.color}*${lines}`)
 }
 
+log.color = '\x1b[35m'
 export default log
